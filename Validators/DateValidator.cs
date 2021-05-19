651e067b6be7a780bbf926b3d2ab518212fc2ce6
El Mahdi <38296191+elmahdi-lab@users.noreply.github.com>
2021-05-19 11:36:01 -0400

    Update DateValidator.cs

--- Validators/DateValidator.cs
+++ Validators/DateValidator.cs
@@ -8,9 +8,6 @@ namespace PomeloHealthApi.Validators
     public static class DateValidator
     {
         private static readonly int[] _minutesAllowed = new int[4] { 0, 15, 30, 45 };
-        private static readonly DatabaseContext _context = new DatabaseContext();
-
-       
 
         public static bool IsValid(IDateEntity entity)
         {
